6ffbb1278b8a85dc4017e79d51c0b4977f2c1493
Vitor Domingos <vm486092@gmail.com>
2023-10-18 21:47:09 -0200

    adicionada a pasta js

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// Exibe a mensagem "Eu amo javascript" no console
+// console.log("Eu amo javascript")
+
+// Exibe um pop-up de confirmação no navegador e armazena a resposta do usuário em 'retornoDoUsuario'
+let retornoDoUsuario = window.confirm("Deseja continuar navegando no site?")
+
+// Exibe no console a resposta do usuário, que é 'true' se o usuário clicou em "OK" ou 'false' se clicou em "Cancelar"
+console.log(retornoDoUsuario)
+
+// Exibe um pop-up de entrada de texto no navegador e armazena a resposta do usuário em 'respostaDoPrompt'
+let respostaDoPrompt = window.prompt("Qual é o seu console favorito?") 
+
+// Exibe no console a resposta do usuário inserida no pop-up
+console.log(respostaDoPrompt)
